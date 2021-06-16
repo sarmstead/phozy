6eb43cf1a13f97ab2ca38ad09f2d837ed68ae2fc
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-06-15 20:36:28 -0600

    Update URIs

--- phozy.php
+++ phozy.php
@@ -1,11 +1,11 @@
 /**
  * Plugin Name:       Phozy
- * Plugin URI:        https://github/sarmstead/phozy/
+ * Plugin URI:        https://githu.com/sarmstead/phozy/
  * Description:       Finally, an efficient way to upload your videos to YouTube directly from WordPress.
  * Version:           0.1.0
  * Requires at least: 5.7.2
  * Author:            Sunjay Armstead
- * Author URI:        https://github/sarmstead/
+ * Author URI:        https://github.com/sarmstead/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
