b5847d88e235050435d83b8956b2de13a1014ed3
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-07-02 13:33:15 -0600

    Create function to parse scripts and load them during init phase

--- includes/phozy-enqueue.php
+++ includes/phozy-enqueue.php
@@ -3,3 +3,16 @@
 
 // If ABSPATH is defined continue script, else stop script
 defined( 'ABSPATH' ) || die( 'Direct script access disallowed.' );
+
+// Run scripts parsing during 'init' phase of WordPress load process (after theme and other plugins are loaded)
+function phozy_parser() {
+    add_filter( 'script_loader_tag', function( $tag, $handle ) {
+        if( !preg_match( '/^phozy-/', $handle ) ) {
+            return $tag;
+        }
+        // Rewrite script tag to load scripts asynchronously
+        return str_replace( ' src', ' async defer src', $tag );
+    }, 10, 2 );
+}
+
+add_action( 'init', phozy_parser() );
