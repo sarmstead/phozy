dee1c23652ae674ebce21401ceacff26b4c6e738
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-07-02 13:02:04 -0600

    Define target node in variable. Conditionally render React app only when target is present.

--- phozy-react/src/index.js
+++ phozy-react/src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('phozy-root')
-);
+// define target element ID for React application
+const phozyTargetNode = document.getElementById('phozy-root');
+
+// Only load app if phozyTargetNode exists
+if (phozyTargetNode) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById(phozyTargetNode)
+  );
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
