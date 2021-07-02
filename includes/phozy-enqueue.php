3bef96d14dc25c4aec90655731ef00d30f16515d
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-07-02 13:12:16 -0600

    Initialize enqueue file

---
+++ includes/phozy-enqueue.php
@@ -0,0 +1,5 @@
+<?php
+// This files enqueues scripts and styles for Phozy's React application
+
+// If ABSPATH is defined continue script, else stop script
+defined( 'ABSPATH' ) || die( 'Direct script access disallowed.' );
