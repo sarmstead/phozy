3853df38b504c429d3dbb361ee36d5dddfbc49c5
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2021-07-02 14:46:47 -0600

    Add forward slash to phozy-enqueue php file path

--- phozy.php
+++ phozy.php
@@ -24,4 +24,4 @@ define ( 'PHOZY_ASSET_MANIFEST', PHOZY_REACT_PATH . '/build/asset-manifest.json'
 define ( 'PHOZY_INCLUDES', plugin_dir_path( __FILE__ ) . '/includes' );
 
 // Require in enqueue file
-require_once( PHOZY_INCLUDES . 'phozy-enqueue.php' );
+require_once( PHOZY_INCLUDES . '/phozy-enqueue.php' );
